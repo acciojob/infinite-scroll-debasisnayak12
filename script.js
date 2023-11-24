4e26757a3566c19dd73de6f80204602a913f6d18
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-24 17:11:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //your code here!
+const container = document.getElementById("infi-list");
 
+let count = 1;
+for(; count <= 10; count++){
+	const listItems = document.createElement("li");
+	listItems.innerText = `Items ${count}`;
+	container.appendChild(listItems);
+}
+
+container.addEventListener(('scroll'), () => {
+	const scrollHeight = container.scrollTop;
+	const maxScrollHeight = container.scrollHeight - container.clientHeight;
+
+	if(maxScrollHeight - scrollHeight <= 5){
+		addTwoMoreItems();
+	}
+});
+
+function addTwoMoreItems() {
+	const listItems1 = document.createElement("li");
+	listItems1.innerText = `Items ${count++}`;
+	
+	const listItems2 = document.createElement("li");
+	listItems2.innerText = `Items ${count++}`;
+
+	container.append(listItems1,listItems2);
+}
